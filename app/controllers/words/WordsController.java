26ebc89f737f8d35174dd05df90a41e686059815
bob <14410558@qq.com>
2019-10-15 08:18:36 +0800

    go

--- app/controllers/words/WordsController.java
+++ app/controllers/words/WordsController.java
@@ -1,39 +1,72 @@
 package controllers.words;
 
-import models.words.Word;
-import models.words.WordTrans;
+import models.words.*;
 import play.mvc.Controller;
 import play.mvc.Http;
 import play.mvc.Result;
+import service.words.ListenWordService;
 import service.words.WordSentenceService;
 import service.words.WordService;
+import service.words.WordTransService;
 import utils.http.RequestUtil;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
-public class TestController extends Controller {
+public class WordsController extends Controller {
 
-    private final WordSentenceService wordSentenceService;
     private final WordService wordService;
+    private final WordTransService wordTransService;
+    private final WordSentenceService wordSentenceService;
+    private final ListenWordService listenWordService;
 
     @Inject
-    public TestController(WordSentenceService wordSentenceService, WordService wordService) {
-        this.wordSentenceService = wordSentenceService;
+    public WordsController(WordService wordService, WordTransService wordTransService, WordSentenceService wordSentenceService, ListenWordService listenWordService) {
         this.wordService = wordService;
+        this.wordTransService = wordTransService;
+        this.wordSentenceService = wordSentenceService;
+        this.listenWordService = listenWordService;
+    }
+
+    public Result listen2Word(Http.Request request) {
+        Long listenId = RequestUtil.getRequestLong(request, "model/listenId").orElse(0L);
+        String wordEns = RequestUtil.getRequestString(request, "model/words").orElse("");
+        List<ListenWord> listenWordList = new ArrayList<>();
+        for (String wordEn: wordEns.split(",")) {
+            ListenWord listenWord = new ListenWord();
+            ListenWordPK pk = new ListenWordPK();
+            pk.listenId = listenId;
+            pk.wordEn = wordEn;
+            listenWord.pk = pk;
+            listenWord.status = true;
+            listenWord.createTime = new Date();
+            listenWordList.add(listenWord);
+        }
+        wordService.checkAndSave(listenId, wordEns.split(","));
+        listenWordService.saveAll(listenWordList);
+        return play.mvc.Results.ok();
+    }
+
+    public Result trans2Py(Http.Request request) {
+        WordPy modelRequest = (WordPy) RequestUtil.getRequestJson(request, "model", WordPy.class);
+        String sortBy = RequestUtil.getRequestString(request, "model/sortBy").orElse("code asc");
+        wordService.trans2Py(modelRequest, sortBy);
+        return play.mvc.Results.ok();
     }
 
     public Result testService(Http.Request request) {
-//        Word modelRequest = (Word) RequestUtil.getRequestJson(request, "model", Word.class);
         WordTrans modelRequest = (WordTrans) RequestUtil.getRequestJson(request, "model", WordTrans.class);
-        String sortBy = RequestUtil.getRequestString(request, "model/sortBy").orElse("code asc");
-        return ok(wordSentenceService.test(modelRequest));
+//        String sortBy = RequestUtil.getRequestString(request, "model/sortBy").orElse("code asc");
+        return ok(wordTransService.get(modelRequest));
     }
 
     public CompletionStage<Result> dict2Db(Http.Request request) {
         Word modelRequest = (Word) RequestUtil.getRequestJson(request, "model", Word.class);
-        String sortBy = RequestUtil.getRequestString(request, "model/sortBy").orElse("code asc");
+        String sortBy = RequestUtil.getRequestString(request, "model/sortBy").orElse("word_en asc");
         wordService.dict2Db(modelRequest, sortBy);
         return CompletableFuture.completedFuture(ok());
     }
--- app/service/words/WordSentenceService.java
+++ app/service/words/WordSentenceService.java
@@ -5,22 +5,22 @@ import play.cache.AsyncCacheApi;
 import play.i18n.MessagesApi;
 import play.libs.concurrent.HttpExecutionContext;
 import play.libs.ws.WSClient;
-import repository.words.WordTransRepository;
+import repository.words.WordSentenceRepository;
 
 import javax.inject.Inject;
 
-public class TestService {
+public class WordSentenceService {
 
     protected final Config config;
     protected final AsyncCacheApi cache;
     protected final WSClient ws;
     protected final HttpExecutionContext httpExecutionContext;
     protected final MessagesApi messagesApi;
-    protected final WordTransRepository repository;
+    protected final WordSentenceRepository repository;
 
     @Inject
-    public TestService(Config config, AsyncCacheApi cache, WSClient ws, HttpExecutionContext httpExecutionContext,
-                       MessagesApi messagesApi, WordTransRepository repository) {
+    public WordSentenceService(Config config, AsyncCacheApi cache, WSClient ws, HttpExecutionContext httpExecutionContext,
+                               MessagesApi messagesApi, WordSentenceRepository repository) {
         this.config = config;
         this.cache = cache;
         this.ws = ws;
