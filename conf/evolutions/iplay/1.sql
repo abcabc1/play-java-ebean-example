1a8cf00681681f1c5e894ffd251decafe889a336
bob <14410558@qq.com>
2019-11-11 10:16:56 +0800

    start with ecommerce

--- conf/evolutions/iplay/1.sql
+++ conf/evolutions/iplay/1.sql
@@ -292,6 +292,36 @@ create table store (
   constraint pk_store primary key (id)
 );
 
+create table store_address (
+  id                            bigint COMMENT 'ID' auto_increment not null,
+  status                        TINYINT UNSIGNED DEFAULT 1 COMMENT '数据是否有效[0 无效,1 有效]' not null,
+  create_time                   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间' not null,
+  update_time                   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '修改时间' not null,
+  store_id                      bigint COMMENT 'ID',
+  address_id                    bigint COMMENT 'ID',
+  constraint pk_store_address primary key (id)
+);
+
+create table store_license (
+  id                            bigint COMMENT 'ID' auto_increment not null,
+  status                        TINYINT UNSIGNED DEFAULT 1 COMMENT '数据是否有效[0 无效,1 有效]' not null,
+  create_time                   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间' not null,
+  update_time                   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '修改时间' not null,
+  store_id                      bigint COMMENT 'ID',
+  license                       varchar(32) COMMENT '配置编码',
+  constraint pk_store_license primary key (id)
+);
+
+create table store_tag (
+  id                            bigint COMMENT 'ID' auto_increment not null,
+  status                        TINYINT UNSIGNED DEFAULT 1 COMMENT '数据是否有效[0 无效,1 有效]' not null,
+  create_time                   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间' not null,
+  update_time                   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '修改时间' not null,
+  store_id                      bigint COMMENT 'ID',
+  tag                           varchar(32) COMMENT '配置编码',
+  constraint pk_store_tag primary key (id)
+);
+
 create table user (
   id                            bigint COMMENT 'ID' auto_increment not null,
   status                        TINYINT UNSIGNED DEFAULT 1 COMMENT '数据是否有效[0 无效,1 有效]' not null,
@@ -306,6 +336,26 @@ create table user (
   constraint uq_user_operator_id unique (operator_id),
   constraint uq_user_code unique (code),
   constraint pk_user primary key (id)
+);
+
+create table user_address (
+  id                            bigint COMMENT 'ID' auto_increment not null,
+  status                        TINYINT UNSIGNED DEFAULT 1 COMMENT '数据是否有效[0 无效,1 有效]' not null,
+  create_time                   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间' not null,
+  update_time                   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '修改时间' not null,
+  user_id                       bigint COMMENT 'ID',
+  address_id                    bigint COMMENT 'ID',
+  constraint pk_user_address primary key (id)
+);
+
+create table user_license (
+  id                            bigint COMMENT 'ID' auto_increment not null,
+  status                        TINYINT UNSIGNED DEFAULT 1 COMMENT '数据是否有效[0 无效,1 有效]' not null,
+  create_time                   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间' not null,
+  update_time                   DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '修改时间' not null,
+  user_id                       bigint COMMENT 'ID',
+  license_code                  varchar(32) COMMENT '配置编码',
+  constraint pk_user_license primary key (id)
 );
 
 create table user_tag (
@@ -361,6 +411,24 @@ alter table merchandise_tag add constraint fk_merchandise_tag_tag foreign key (t
 
 alter table store add constraint fk_store_operator_id foreign key (operator_id) references operator (id) on delete restrict on update restrict;
 
+create index ix_store_address_store_id on store_address (store_id);
+alter table store_address add constraint fk_store_address_store_id foreign key (store_id) references store (id) on delete restrict on update restrict;
+
+create index ix_store_address_address_id on store_address (address_id);
+alter table store_address add constraint fk_store_address_address_id foreign key (address_id) references address (id) on delete restrict on update restrict;
+
+create index ix_store_license_store_id on store_license (store_id);
+alter table store_license add constraint fk_store_license_store_id foreign key (store_id) references store (id) on delete restrict on update restrict;
+
+create index ix_store_license_license on store_license (license);
+alter table store_license add constraint fk_store_license_license foreign key (license) references config (code) on delete restrict on update restrict;
+
+create index ix_store_tag_store_id on store_tag (store_id);
+alter table store_tag add constraint fk_store_tag_store_id foreign key (store_id) references store (id) on delete restrict on update restrict;
+
+create index ix_store_tag_tag on store_tag (tag);
+alter table store_tag add constraint fk_store_tag_tag foreign key (tag) references config (code) on delete restrict on update restrict;
+
 alter table user add constraint fk_user_operator_id foreign key (operator_id) references operator (id) on delete restrict on update restrict;
 
 create index ix_user_level on user (level);
@@ -368,6 +436,18 @@ alter table user add constraint fk_user_level foreign key (level) references con
 
 create index ix_user_type on user (type);
 alter table user add constraint fk_user_type foreign key (type) references config (code) on delete restrict on update restrict;
+
+create index ix_user_address_user_id on user_address (user_id);
+alter table user_address add constraint fk_user_address_user_id foreign key (user_id) references user (id) on delete restrict on update restrict;
+
+create index ix_user_address_address_id on user_address (address_id);
+alter table user_address add constraint fk_user_address_address_id foreign key (address_id) references address (id) on delete restrict on update restrict;
+
+create index ix_user_license_user_id on user_license (user_id);
+alter table user_license add constraint fk_user_license_user_id foreign key (user_id) references user (id) on delete restrict on update restrict;
+
+create index ix_user_license_license_code on user_license (license_code);
+alter table user_license add constraint fk_user_license_license_code foreign key (license_code) references config (code) on delete restrict on update restrict;
 
 create index ix_user_tag_user_id on user_tag (user_id);
 alter table user_tag add constraint fk_user_tag_user_id foreign key (user_id) references user (id) on delete restrict on update restrict;
@@ -421,6 +501,24 @@ drop index ix_merchandise_tag_tag on merchandise_tag;
 
 alter table store drop foreign key fk_store_operator_id;
 
+alter table store_address drop foreign key fk_store_address_store_id;
+drop index ix_store_address_store_id on store_address;
+
+alter table store_address drop foreign key fk_store_address_address_id;
+drop index ix_store_address_address_id on store_address;
+
+alter table store_license drop foreign key fk_store_license_store_id;
+drop index ix_store_license_store_id on store_license;
+
+alter table store_license drop foreign key fk_store_license_license;
+drop index ix_store_license_license on store_license;
+
+alter table store_tag drop foreign key fk_store_tag_store_id;
+drop index ix_store_tag_store_id on store_tag;
+
+alter table store_tag drop foreign key fk_store_tag_tag;
+drop index ix_store_tag_tag on store_tag;
+
 alter table user drop foreign key fk_user_operator_id;
 
 alter table user drop foreign key fk_user_level;
@@ -428,6 +526,18 @@ drop index ix_user_level on user;
 
 alter table user drop foreign key fk_user_type;
 drop index ix_user_type on user;
+
+alter table user_address drop foreign key fk_user_address_user_id;
+drop index ix_user_address_user_id on user_address;
+
+alter table user_address drop foreign key fk_user_address_address_id;
+drop index ix_user_address_address_id on user_address;
+
+alter table user_license drop foreign key fk_user_license_user_id;
+drop index ix_user_license_user_id on user_license;
+
+alter table user_license drop foreign key fk_user_license_license_code;
+drop index ix_user_license_license_code on user_license;
 
 alter table user_tag drop foreign key fk_user_tag_user_id;
 drop index ix_user_tag_user_id on user_tag;
@@ -477,7 +587,17 @@ drop table if exists pack;
 
 drop table if exists store;
 
+drop table if exists store_address;
+
+drop table if exists store_license;
+
+drop table if exists store_tag;
+
 drop table if exists user;
+
+drop table if exists user_address;
+
+drop table if exists user_license;
 
 drop table if exists user_tag;
 
